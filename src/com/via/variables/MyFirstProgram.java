66633f468e94766fe500b7c285e7144cf73c7a07
Val Alberto <va.alberto@fujitsu.com>
2022-11-15 15:26:15 +0800

    section2: printing strings with the format method in Java

--- src/com/via/variables/MyFirstProgram.java
+++ src/com/via/variables/MyFirstProgram.java
@@ -2,8 +2,13 @@ package com.via.variables;
 
 public class MyFirstProgram {
 	
+	public static int staticVar = 20;
+	public static String name = "Smith";
+	
 	public static void main(String[] args) {
 		
+		
+	//######## section1 ############	
 		//overriding value assignment
 		int varInt = 15;
 		varInt = 100;
@@ -14,5 +19,17 @@ public class MyFirstProgram {
 		final int maxRating = 10;
 //		maxRating = 100;
 		System.out.println(maxRating);
+		
+		
+	//####### section2 #############		
+		String varString = "This is string";
+		System.out.println("The string value: " + varString);
+		
+		int localVar = 10;
+		System.out.println("Static variable value is: " + staticVar);
+		System.out.println("Local variable value is: " + localVar);
+		
+		int salary = 50000;
+		System.out.println(name  +  " Salary is: " + salary);
 	}
 }
