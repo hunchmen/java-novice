0fe62d3ceafda1113e7c66a91211892c0307593f
hunchmen <valalberto0214@gmail.com>
2022-11-28 21:36:49 +0800

    section4: Logical Operators

---
+++ src/com/via/course4/operators/LogicalOperators.java
@@ -0,0 +1,28 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2022. All Rights Reserved.
+ * 
+ */
+package com.via.course4.operators;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 28 Nov 2022
+ */
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        boolean isJavaFun = true;
+        boolean isCoffeeBitter = true;
+        boolean isTheSkyPurple = false;
+        boolean isTheEarthFlat = false;
+
+        // OR Operator -> when there is true operand will return: true
+        // AND operator -> when operand is both true will return: true
+    }
+
+}
