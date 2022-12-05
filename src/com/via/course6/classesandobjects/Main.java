5aa067d56cb35954dcce03e3066704f118671c47
hunchmen <valalberto0214@gmail.com>
2022-12-05 20:46:56 +0800

    section5: creating objet of classes

---
+++ src/com/via/course6/classesandobjects/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2022. All Rights Reserved.
+ * 
+ */
+package com.via.course6.classesandobjects;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 5 Dec 2022
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String someString = new String("Hello String class!");
+        System.out.println("Some string: " + someString);
+
+        String anotherString = new String("Another object of type string");
+        System.out.println("Another string: " + anotherString);
+
+        System.out.println();
+    }
+}
