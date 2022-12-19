3758e362a199a554b062a52cb345e033644d3273
hunchmen <valalberto0214@gmail.com>
2022-12-19 17:11:26 +0800

    section5: understanding and using default constructor

---
+++ src/com/via/course8/methods/Car.java
@@ -0,0 +1,25 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2022. All Rights Reserved.
+ * 
+ */
+package com.via.course8.methods;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 19 Dec 2022
+ */
+public class Car {
+
+    public String name;
+    public String type;
+    public float mpg;
+    public int price;
+
+    public Car() {
+
+    }
+}
